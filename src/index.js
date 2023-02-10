d7b445de6daf85dd347b6c4be87ae3bce2f1ec53
mahesh <mahesh.kchm@lendenclub.com>
2023-02-10 11:58:55 +0545

    feat: added initial react-query setup

--- src/index.js
+++ src/index.js
@@ -8,11 +8,40 @@ import RTLLayout from "layouts/rtl";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "theme/theme";
 import { ThemeEditorProvider } from "@hypertheme-editor/chakra-ui";
+import { QueryCache, QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { toastFail } from "components/toast";
+
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: true,
+      staleTime: 30 * 1000,
+    },
+  },
+  queryCache: new QueryCache({
+    onError: async error => {
+      if (error.request?.status === 401) {
+        // queryClient.setQueryData(authTokenKey, () => false);
+        setTimeout(() => {
+          //clear token here
+          queryClient.clear();
+          toastFail("Session Expired! Please login again!");
+        }, 500);
+      }
+    },
+  }),
+});
 
 ReactDOM.render(
   <ChakraProvider theme={theme}>
     <React.StrictMode>
       <ThemeEditorProvider>
+      <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
         <HashRouter>
           <Switch>
             <Route path={`/auth`} component={AuthLayout} />
@@ -21,6 +50,7 @@ ReactDOM.render(
             <Redirect from='/' to='/admin' />
           </Switch>
         </HashRouter>
+        </QueryClientProvider>
       </ThemeEditorProvider>
     </React.StrictMode>
   </ChakraProvider>,
---
+++ src/service/login.js
@@ -0,0 +1,52 @@
+
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { api } from "./service-api";
+import { MCPHttpClient } from "./service-axios";
+import { toastFail } from "components/toast";
+import { toastSuccess } from "components/toast";
+
+const fetchClientUser = (clientId) => () => {
+  return MCPHttpClient.get(
+    api.login,
+    {
+      params: { clientId: clientId },
+    }
+  );
+};
+
+const useFetchClientUser = (clientId) => {
+  return useQuery(
+    [api.login, clientId],
+    fetchClientUser(clientId),
+    {
+      enabled: !!clientId,
+      select: data => data?.data?.data,
+    }
+  );
+};
+
+const saveClientUser = () => {
+  return MCPHttpClient.post(
+    api.login,
+    {}
+  );
+};
+
+const useSaveClientUser = () => {
+  const queryClient = useQueryClient();
+  return useMutation(saveClientUser, {
+    onError: (error) => {
+      toastFail(error?.response?.data?.message ?? "");
+    },
+    onSuccess: success => {
+      toastSuccess(success?.data?.message);
+      queryClient.invalidateQueries(api.clientUser.clientUser);
+    },
+  });
+};
+
+
+export {
+  useFetchClientUser,
+  useSaveClientUser,
+};
