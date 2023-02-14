3c3aa73f9f3242cb6ab5cf0ab74e4f9b75529b96
mahesh <maheshkhatiwada17@gmail.com>
2023-02-14 23:05:49 +0545

    feat: added dropzone

--- src/views/onboarding/UploadDocuments.jsx
+++ src/views/onboarding/UploadDocuments.jsx
@@ -1,23 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AccordionButton,
   AccordionIcon,
   AccordionItem,
   AccordionPanel,
   Box,
-  Button,
-  Flex,
-  Icon,
-  SimpleGrid,
-  Text,
   useColorModeValue,
 } from "@chakra-ui/react";
 import {yupResolver} from "@hookform/resolvers/yup";
 import * as Yup from "yup";
 import {useForm} from "react-hook-form";
-import { MdUpload } from "react-icons/md";
-import Dropzone from "views/admin/profile/components/Dropzone";
-import Card from "components/card/Card";
+import DropzoneComponent from "components/Dropzone";
+
 
 const verifySchema = Yup.object().shape({
   mobile_number: Yup.string()
@@ -37,6 +31,7 @@ const verifySchema = Yup.object().shape({
 });
 
 const UploadDocument = () => {
+  const [acceptedFiles, setAcceptedFiles] = useState([]);
   const brandColor = useColorModeValue("brand.500", "white");
   const verifyDefaultValues = {
     mobile_number: "",
@@ -68,40 +63,11 @@ const UploadDocument = () => {
       </h2>
       <AccordionPanel pb={4}>
         <form onSubmit={handleSubmit(onSubmitHandler)}>
-        <Card mb='20px' align='center' p='20px'>
-          <Dropzone
-          w={{ base: "100%", "2xl": "268px" }}
-          me='36px'
-          maxH={{ base: "60%", lg: "50%", "2xl": "100%" }}
-          minH={{ base: "60%", lg: "50%", "2xl": "100%" }}
-          content={
-            <Box>
-              <Icon as={MdUpload} w='80px' h='80px' color={brandColor} />
-              <Flex justify='center' mx='auto' mb='12px'>
-                <Text fontSize='xl' fontWeight='700' color={brandColor}>
-                  Upload Files
-                </Text>
-              </Flex>
-              <Text fontSize='sm' fontWeight='500' color='secondaryGray.500'>
-                PNG, JPG and GIF files are allowed
-              </Text>
-            </Box>
-          }
-        />
-        </Card>
-{/*           
-          <Button
-            fontSize="sm"
-            variant="brand"
-            fontWeight="500"
-            w={{sm:"100%", lg:"30%"}}
-            h="50"
-            mt="5px"
-            mb="24px"
-            type="submit"
-          >
-            Save and Continue
-          </Button> */}
+      <DropzoneComponent
+        setAcceptedFiles={setAcceptedFiles}
+        helperText="Please drop file"
+      />
+
         </form>
         
       </AccordionPanel>
